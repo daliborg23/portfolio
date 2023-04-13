b7d2a5be2419b64caa5bf254bcfae2a63bd2f6ab
daliborg23 <116153885+daliborg23@users.noreply.github.com>
2023-04-13 22:06:17 +0200

    add new project

--- poslat-mail.php
+++ poslat-mail.php
@@ -1,18 +1,14 @@
 <?php
-$to = 'gldalibor@gmail.com';
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$message .= "\r\n\r\n" . "Jmeno: " . $name . "\r\n" . "Email: " . $email;
-
-mail(
-    $to,
-    $subject,
-    $message,
-    $additional_headers,
-);
-
-header('Location:http://daliborg.wz.cz/#contact');
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$to = 'glda@protonmail.ch';
+$from = 'email@daliborg.8u.cz';
+$name = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$subject = addslashes($_POST['subject']);
+$message = 'Jmeno odesilatele: ' . $name . "\r\n" . 'Email: ' . $email;
+$message .= "\r\n" . addslashes($_POST['message']);
+$headers = 'From:' . $from;
+mail($to, $subject, $message, $headers);
+header('Location:http://daliborg.8u.cz/#contact');
 ?>
